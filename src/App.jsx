ec43749c437ec0348193c6a65aa88d90f21019c9
ezraamanoe <ezramanoe59@gmail.com>
2025-04-05 12:12:12 +0100

    making hero

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { MorphingText } from "@/components/magicui/morphing-text";
 import {NavBar} from "@/components/ui/navbar"
 import { cn } from "@/lib/utils";
 import { DotPattern } from "@/components/magicui/dot-pattern";
+import { Particles } from "@/components/magicui/particles";
 
 const texts = [
   "Hello",
@@ -20,32 +21,33 @@ function App() {
 
   return (
     <>
-    
-    <DotPattern
-        glow={true}
-        className={cn(
-          "[mask-image:radial-gradient(500px_circle_at_center,white,transparent)]",
-        )}
-      />
     <div className="navbar">
       <NavBar />
     </div>
 
     <div className="hero-text">
-      <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
-        Meteors
+
+      <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-500/80 bg-clip-text text-center text-9xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
+        Mock Mate
       </span>
       <div className="hero-text-subtitle">
-        <p className="text-sm text-muted-foreground">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
-          exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore 
-          eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-          sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <p className="text-sm">
+        Practice real-time interviews with an AI 
+        that talks back. MockMate simulates voice-powered 
+        interview conversations to help you gain confidence, 
+        improve answers, and get hired.  
         </p>
       </div>
     </div>
+    <Particles
+        className="absolute inset-0 -z-10 h-full w-full"
+        quantity={400}
+        ease={80}
+        color={"#000000"}
+        vx={0.5}
+        vy={0.5}
+        refresh
+      />
     </>
 
   )
